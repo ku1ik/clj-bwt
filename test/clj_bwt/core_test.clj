b06f6c5256a83335bfa77fc68b034289720ec856
Marcin Kulik <m@ku1ik.com>
2016-12-08 19:26:11 +0100

    Add property test for reversiveness

--- test/clj_bwt/core_test.clj
+++ test/clj_bwt/core_test.clj
@@ -1,6 +1,11 @@
 (ns clj-bwt.core-test
   (:require [clojure.test :refer :all]
-            [clj-bwt.core :refer :all]))
+            [clj-bwt.core :refer :all]
+            [clojure.test.check :as tc]
+            [clojure.test.check.generators :as gen]
+            [clojure.test.check.properties :as prop]
+            [clojure.test.check.clojure-test :refer [defspec]]
+            [clojure.string :as str]))
 
 (deftest rotations-test
   (is (= (rotations [1 2 3]) '((1 2 3) (2 3 1) (3 1 2)))))
@@ -12,3 +17,9 @@
 (deftest reverse-bwt-test
   (is (= (reverse-bwt (bwt "The rain in Spain stays mainly in the plain" \$) \$)
          "The rain in Spain stays mainly in the plain")))
+
+(defspec test-bwt-and-reverse
+  100
+  (prop/for-all
+   [s (gen/such-that #(not (str/includes? % "$")) gen/string)]
+   (= (reverse-bwt (bwt s \$) \$) s)))
